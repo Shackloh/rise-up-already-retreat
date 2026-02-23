1f08a20f14cb18dff3cd34501f625c4d46de64c4
Shackloh <146730506+Shackloh@users.noreply.github.com>
2026-02-22 20:27:25 -0500

    Add Tailwind CSS configuration file
    Configure Tailwind CSS with custom colors and content paths.

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'forest': {
+          50: 'f0f4ed',
+          500: '2d5016',
+          600: '1e3a0a',
+        },
+        'parchment': {
+          50: 'fffef9',
+          100: 'fefdf5',
+          200: 'fef9eb',
+          500: 'f5f1e8',
+        },
+        'gold': {
+          500: 'd4a574',
+          600: 'c89563',
+        },
+      },
+    },
+  },
+  plugins: [],
+}  
