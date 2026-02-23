7f57133237725c61319e4793edb7ae7b1a0c318a
Shackloh <146730506+Shackloh@users.noreply.github.com>
2026-02-22 21:06:27 -0500

    Simplify App component with retreat heading
    Removed multiple section components and added a heading for the retreat.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,7 @@
-import RetreatNavbar from './components/RetreatNavbar'
-import HeroSection from './components/HeroSection'
-import HookSection from './components/HookSection'
-import ExperienceSection from './components/ExperienceSection'
-import TargetAudienceSection from './components/TargetAudienceSection'
-import ScheduleSection from './components/ScheduleSection'
-import LocationSection from './components/LocationSection'
-import PricingSection from './components/PricingSection'
-import FAQSection from './components/FAQSection'
-import CTASection from './components/CTASection'
-import RetreatFooter from './components/RetreatFooter'
-
 function App() {
   return (
-    <div className="min-h-screen bg-white">
-      <RetreatNavbar />
-      <HeroSection />
-      <HookSection />
-      <ExperienceSection />
-      <TargetAudienceSection />
-      <ScheduleSection />
-      <LocationSection />
-      <PricingSection />
-      <FAQSection />
-      <CTASection />
-      <RetreatFooter />
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Rise Up Already - Retreat Coming Soon</h1>
     </div>
   )
 }
