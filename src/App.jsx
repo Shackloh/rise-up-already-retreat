657b9e5a317d6a3372527ec0e94d03f3e5c3fc8b
Shackloh <146730506+Shackloh@users.noreply.github.com>
2026-02-22 20:55:29 -0500

    Add App.jsx component

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import RetreatNavbar from './components/RetreatNavbar'
+import HeroSection from './components/HeroSection'
+import HookSection from './components/HookSection'
+import ExperienceSection from './components/ExperienceSection'
+import TargetAudienceSection from './components/TargetAudienceSection'
+import ScheduleSection from './components/ScheduleSection'
+import LocationSection from './components/LocationSection'
+import PricingSection from './components/PricingSection'
+import FAQSection from './components/FAQSection'
+import CTASection from './components/CTASection'
+import RetreatFooter from './components/RetreatFooter'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <RetreatNavbar />
+      <HeroSection />
+      <HookSection />
+      <ExperienceSection />
+      <TargetAudienceSection />
+      <ScheduleSection />
+      <LocationSection />
+      <PricingSection />
+      <FAQSection />
+      <CTASection />
+      <RetreatFooter />
+    </div>
+  )
+}
+
+export default App
