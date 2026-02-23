7147f5773179c1517d9f28578624150069cbcb7b
Shackloh <146730506+Shackloh@users.noreply.github.com>
2026-02-22 21:27:26 -0500

    Refactor App component to include multiple sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import React from 'react';
+import RetreatNavbar from './components/RetreatNavbar';
+import HeroSection from './components/HeroSection';
+import HookSection from './components/HookSection';
+import ExperienceSection from './components/ExperienceSection';
+import TargetAudienceSection from './components/TargetAudienceSection';
+import ScheduleSection from './components/ScheduleSection';
+import LocationSection from './components/LocationSection';
+import PricingSection from './components/PricingSection';
+import FAQSection from './components/FAQSection';
+import CTASection from './components/CTASection';
+import RetreatFooter from './components/RetreatFooter';
+
 function App() {
   return (
-    <div className="min-h-screen bg-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Rise Up Already - Retreat Coming Soon</h1>
+    <div className="bg-white">
+      <RetreatNavbar />
+      <HeroSection />
+      <HookSection />
+      <ExperienceSection />
+      <TargetAudienceSection />
+      <ScheduleSection />
+      <LocationSection />
+      <PricingSection />
+      <FAQSection />
+      <CTASection />
+      <RetreatFooter />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
